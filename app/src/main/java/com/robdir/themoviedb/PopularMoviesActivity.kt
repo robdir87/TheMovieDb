8ca2159b0ec9de381228f89a5ef51d388cad0c7e
robdir <robertodirusso@gmail.com>
2018-09-23 19:45:29 +0100

    Initializes the project

---
+++ app/src/main/java/com/robdir/themoviedb/PopularMoviesActivity.kt
@@ -0,0 +1,12 @@
+package com.robdir.themoviedb
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class PopularMoviesActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_popular_movies)
+    }
+}

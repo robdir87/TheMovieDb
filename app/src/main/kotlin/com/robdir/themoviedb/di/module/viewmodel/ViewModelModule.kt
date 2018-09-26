393a8a569c98d5e42610b22a29b4319966723bca
robdir <robertodirusso@gmail.com>
2018-09-26 23:35:19 +0100

    Adds MovieDetailViewModel to DI

--- app/src/main/kotlin/com/robdir/themoviedb/di/module/viewmodel/ViewModelModule.kt
+++ app/src/main/kotlin/com/robdir/themoviedb/di/module/viewmodel/ViewModelModule.kt
@@ -2,6 +2,7 @@ package com.robdir.themoviedb.di.module.viewmodel
 
 import android.arch.lifecycle.ViewModel
 import android.arch.lifecycle.ViewModelProvider
+import com.robdir.themoviedb.presentation.moviedetails.MovieDetailViewModel
 import com.robdir.themoviedb.presentation.movielists.popularmovies.PopularMoviesViewModel
 import dagger.Binds
 import dagger.Module
@@ -14,6 +15,11 @@ abstract class ViewModelModule {
     @IntoMap
     @ViewModelKey(PopularMoviesViewModel::class)
     abstract fun bindPopularMoviesViewModel(popularMoviesViewModel: PopularMoviesViewModel): ViewModel
+
+    @Binds
+    @IntoMap
+    @ViewModelKey(MovieDetailViewModel::class)
+    abstract fun bindMovieDetailViewModel(movieDetailViewModel: MovieDetailViewModel): ViewModel
 
     @Binds
     abstract fun bindViewModelFactory(viewModelFactory: ViewModelFactory): ViewModelProvider.Factory

8d54ceee92895024cb8b55e88d703ec3b6eebc08
robdir <robertodirusso@gmail.com>
2018-09-27 18:10:15 +0100

    Adds DAOs to DI structure

--- app/src/main/kotlin/com/robdir/themoviedb/di/module/PersistenceModule.kt
+++ app/src/main/kotlin/com/robdir/themoviedb/di/module/PersistenceModule.kt
@@ -4,6 +4,8 @@ import android.arch.persistence.room.Room
 import android.content.Context
 import com.robdir.themoviedb.data.persistence.AppDatabase
 import com.robdir.themoviedb.data.persistence.DATABASE_NAME
+import com.robdir.themoviedb.data.persistence.GenresDao
+import com.robdir.themoviedb.data.persistence.PopularMoviesDao
 import dagger.Module
 import dagger.Provides
 
@@ -15,4 +17,13 @@ object PersistenceModule {
     fun provideDatabase(context: Context): AppDatabase =
         Room.databaseBuilder(context, AppDatabase::class.java, DATABASE_NAME)
             .build()
+
+    @Provides
+    @JvmStatic
+    fun providePopularMoviesDao(database: AppDatabase): PopularMoviesDao =
+        database.getPopularMoviesDao()
+
+    @Provides
+    @JvmStatic
+    fun provideGenresDao(database: AppDatabase): GenresDao = database.getGenresDao()
 }
